a176fb3a56bbecbfeb8609b235f48fb45f1b9d63
yunerou <yunerou yunerou@gmail.com>
2026-03-30 16:01:44 +0700

    feat(repo/postgres): implement UpdateStatusTransferring

---
+++ core/repository/impl-postgres/active_conn/update_status_transferring.go
@@ -0,0 +1,31 @@
+package activeConnRepo
+
+import (
+	"context"
+
+	voWs "github.com/pipewave-dev/go-pkg/core/domain/value-object/ws"
+	"github.com/pipewave-dev/go-pkg/pkg/observer"
+	"github.com/pipewave-dev/go-pkg/shared/aerror"
+)
+
+const fnUpdateStatusTransferring = "activeConnRepo.UpdateStatusTransferring"
+
+func (r *activeConnRepo) UpdateStatusTransferring(ctx context.Context, userID string, instanceID string) (aErr aerror.AError) {
+	var op observer.Operation
+	ctx, op = r.obs.StartOperation(ctx, fnUpdateStatusTransferring)
+	defer op.Finish(aErr)
+
+	query := `
+		UPDATE active_connections
+		SET status = $1, holder_id = ''
+		WHERE user_id = $2 AND session_id = $3
+	`
+
+	_, err := r.pool.Exec(ctx, query, voWs.WsStatusTransferring, userID, instanceID)
+	if err != nil {
+		aErr = aerror.New(ctx, aerror.ErrUnexpectedDatabase, err)
+		return aErr
+	}
+
+	return nil
+}
